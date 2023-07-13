416288a67c5617232bdfff1ad26d3c5e01dee712
gitirana <thaygitirana@gmail.com>
2023-07-12 21:53:29 -0300

    chore: create theme provider for dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+"use-client";
+
 import { Chat } from "@/components/Chat";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-slate-50 items-center justify-center">
-      <Chat />
-    </div>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <div className="flex min-h-screen bg-slate-100 dark:bg-slate-900 items-center justify-center">
+        <Chat />
+      </div>
+    </ThemeProvider>
   );
 }
